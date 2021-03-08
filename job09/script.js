3fdce35d4d07d135d5496dcbf33a5e83b07331c0
Mathieu Duverne <mathieu.duverne@laplateforme.io>
2021-03-08 22:08:29 +0100

    jobs done

--- job09/script.js
+++ job09/script.js
@@ -1,15 +1,29 @@
-function tri(numbers){
-        for(let a = 0;a <= numbers.length;a++) {
-            console.log("yes");
-            if(numbers[a+1] < numbers[a])
-            {
-                let easy = numbers[a];
-                numbers[a] = numbers[a+1];
-                numbers[a+1] = easy;
-                tri(numbers);
-            }
+function tri(numbers, order){
+    console.log("oui");
+    for(let a = 0;a <= numbers.length;a++)
+    {
+        if(order=="asc"){
+        if(numbers[a+1] < numbers[a]){
+            let order = "asc";
+            let easy = numbers[a];
+            numbers[a] = numbers[a+1];
+            numbers[a+1] = easy;
+            console.log(numbers[a]);
+            tri(numbers, order);
         }
-        return numbers;
     }
+    if(order=="desc"){
+        if(numbers[a+1] > numbers[a]){
+            let order = "desc";
+            let easy = numbers[a];
+            numbers[a] = numbers[a+1];
+            numbers[a+1] = easy;
+            tri(numbers, order);
+        }
+    }
+}
+    return numbers;
+}
 let numbers = [3,6,2,1,5];
-document.write(tri(numbers));
+let order = "asc";
+document.write(tri(numbers, order)); 
