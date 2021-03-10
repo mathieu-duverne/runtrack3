f190b8b5cfb6663da11b5a0f1d4e01ce3dfcf70c
mathieu-duverne <mathieu.duverne@laplateforme.io>
2021-03-10 16:28:15 +0100

    all jobs done except 3

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -8,8 +8,32 @@
     <title>Job02</title>
 </head>
 <body>
-<button id="button">Arc</button>
+<button id="button">Melange</button>
+<section  style="display: flex; flex-direction: column;">
+<div id="shuffle" style="display: flex; flex-direction: row;" >
+    <div id="div1">
+        <img  id="1" src="images/arc1.png" alt="">
+    </div>
+    <div id="div2">
+        <img id="2" src="images/arc2.png" alt="">
+    </div>
+    <div id="div3">
+        <img id="3" src="images/arc3.png" alt="">
+    </div>
+    <div id="div4">
+        <img id="4" src="images/arc4.png" alt="">
+    </div>
+    <div id="div5">
+        <img id="5" src="images/arc5.png" alt="">
+    </div>
+    <div id="div6">
+        <img id="6" src="images/arc6.png" alt="">
+            </div>
+</div>
+<div id="rangee" style="display: flex; flex-direction: row;">
 
+</div>
+</section>
 <script
     src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
     integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
---
+++ jour03/job02/script.js
@@ -0,0 +1,26 @@
+let cloud = '';
+$("#button").click(function () {
+    var parent = $("#shuffle");
+    const divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+});
+
+
+$('img').click(function(event){
+    cloud += event.target.id;
+    // console.log(cloud);
+    $("#rangee").append(this);
+    console.log(cloud.length);
+    // console.log(cloud);
+    if(cloud.length==6)
+    {
+        if(cloud == 123456)
+        {
+            document.write("<h1 style='color: green'>Tu as gagné Bravo Quelle jolie arc en ciel !!</h1>");
+        }
+        else
+            document.write("<h1 style='color: red'>Tu as perdu !! Recommence</h1>");
+    }
+});
