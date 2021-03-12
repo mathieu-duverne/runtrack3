3fa2af737a807bc0fc44a8abbbccd8fce6460a5a
mathieu-duverne <mathieu.duverne@laplateforme.io>
2021-03-12 18:12:59 +0100

    job1 in work

---
+++ jour05/inscription.php
@@ -0,0 +1,80 @@
+<?php
+echo 'test';
+//try
+//{
+//    $conn = new PDO("mysql:host=localhost;dbname=utilisateurs", "root", "");
+//// set the PDO error mode to exception
+//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    echo "Connected successfully"."<br>";
+//}
+//catch
+//(PDOException $e) {
+//    echo "Connection failed: " . $e->getMessage();
+//}
+//var_dump($_POST);
+//if(isset($_POST['inscription']))
+//{
+//    if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['password_hash']))
+//    {
+//        echo "CHAMPS VIDE ! IN PHP";
+//    }
+//    else {
+//        if ($_POST['password'] == $_POST['password_hash']) {
+//            $sql = "INSERT INTO utilisateurs SET nom = :nom, prenom = :prenom, email = :email, password = :password";
+//            $stmt = $conn->prepare($sql);
+//            $stmt->execute([
+//                "nom" => $_POST['nom'],
+//                "prenom" => $_POST['prenom'],
+//                "email" => $_POST['email'],
+//                "password" => $_POST['password']
+//            ]);
+//        }
+//    }
+//}
+//?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inscription</title>
+</head>
+<body>
+<form action="inscription.php" method="post">
+    Nom
+    <input id="nom" name="nom" type="text"><br>
+    <div id="error_nom">
+
+    </div>
+    Prenom
+    <input id="prenom" name="prenom" type="text"><br>
+    <div id="error_prenom">
+
+    </div>
+    Email
+    <input id="email" name="email" type="email"><br>
+    <div id="error_email">
+
+    </div>
+    Password
+    <input id="password" name="password" type="password"><br>
+    Verif Password
+    <input id="password_hash" type="password" name="password_hash"><br>
+    <div id="error_password">
+
+    </div>
+    <button type="button" id="recuperation">envoyer</button><br>
+    <div id="error_empty">
+
+    </div>
+</form>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
+<?php
+var_dump($_POST);
+?>
+
---
+++ jour05/script.js
@@ -0,0 +1,53 @@
+
+function ValidateEmail(mail)
+{
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+    {
+        return true
+    }
+    return false
+}
+var success = [];
+
+$('#recuperation').click( function (event){
+    var nom = $("#nom").val();
+    var prenom = $('#prenom').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var password_hash = $('#password_hash').val();
+    $("#error_empty").empty();
+    $("#error_email").empty();
+    $("#error_password").empty();
+    // console.log("yes");
+    var error = [];
+    if(nom == "" || prenom == "" || email == "" || password == "" || password_hash == "")
+    {
+        if($('#error').children().length == 0){
+            error['empty'] = "<p style='color: red'>Champs Vide !</p>";
+            $("#error_empty").append(error['empty']);
+        }
+    }
+    if(ValidateEmail(email)===false)
+    {
+        error['email'] = "<p style='color: red'>Email non valide</p>";
+        $("#error_email").append(error['email']);
+    }
+    if(password!=password_hash)
+    {
+        error['password'] = "<p style='color: red'>Password non valide !</p>";
+        $("#error_password").append(error['password']);
+    }
+    else {
+console.log('test');
+    $.ajax({
+            url: 'inscription.php',
+            type: 'POST',
+            data:{nom:nom,prenom:prenom,email:email,password:password,password_hash:password_hash},
+            dataType: 'text',
+        })
+            .done(function(data){
+                console.log(data);
+            })
+    }
+});
+
