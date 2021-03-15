6f2dc7316b1ed684a74078dd51fe71377a5eedf2
mathieu-duverne <mathieu.duverne@laplateforme.io>
2021-03-15 20:14:07 +0100

    jobs4 not done

---
+++ jour06/job03/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Job03</title>
+</head>
+<body>
+<section class="section" > </section>
+<section class="section"> </section>
+<section> </section>
+<section></section>
+</body>
+</html>
---
+++ jour06/job04/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Job04</title>
+</head>
+<body id="body">
+<header id="header">
+    <div id="z"></div>
+    <a id="a" href="">Acueil</a>
+    <div id="z"></div>
+    <a id="a" href="">Présentation</a>
+    <div id="z"></div>
+    <a id="a" href="">Contact</a>
+</header>
+<div id="remplie">
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $('#header').click(function(event){
+        document.body.style.backgroundColor = "grey";
+        $("#remplie").append("<a id='e' href=''>Acueil</a>");
+        $("#remplie").append("<a id='e' href=''>Présentation</a>");
+        $("#remplie").append("<a id='e' href=''>Contact</a>");
+    });
+</script>
+</body>
+</html>
